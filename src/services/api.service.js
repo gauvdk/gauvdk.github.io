c446909206b737815b32747f0df217daa19042df
trackoad <cyril.besse.97@gmail.com>
2022-02-13 00:58:26 +0100

    add login

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,6 +1,10 @@
 export const ApiService = {};
-
+// 
+// const _url = 'http://api.skimfrance.fr:3563';
 const _url = 'https://api.skimfrance.fr';
+// const _url = 'http://localhost:3563';
+
+let tokenSession;
 
 /**
  * @param {string} type
@@ -8,4 +12,14 @@ const _url = 'https://api.skimfrance.fr';
 ApiService.getAll = type => {
 	return fetch(_url + '/api/' + type)
 		.then(a => a.json());
+};
+
+ApiService.login = async password => {
+	await axios.post(_url + '/login', { password }, {
+		hearder: {
+			'Access-Control-Allow-Headers': '*'
+		}
+	})
+		.then(a => tokenSession = a.data);
+	return Boolean(tokenSession);
 };
