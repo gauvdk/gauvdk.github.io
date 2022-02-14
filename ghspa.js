2cf3953fc007b7135446df53399cf15f6b6b4046
trackoad <cyril.besse.97@gmail.com>
2022-02-14 19:38:50 +0100

    fix path

--- ghspa.js
+++ ghspa.js
@@ -24,10 +24,7 @@
 
 	/* redirect all 404 trafic to index.html */
 	function redirect() {
-		l.replace(l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + repo + '/?' +
-			(l.pathname ? 'p=' + l.pathname.replace(/&/g, '~and~').replace(repo, '') : '') +
-			(l.search ? '&q=' + l.search.slice(1).replace(/&/g, '~and~') : '') +
-			(l.hash))
+		l.replace(`${l.protocol}//${l.hostname}?p=${l.pathname.substring(1)}`);
 	}
 
 	/* resolve 404 redirects into internal routes */
