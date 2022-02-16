ce3ec42e40511ae550503a94ed69e0b7182d3442
trackoad <cyril.besse.97@gmail.com>
2022-02-16 20:18:49 +0100

    add efit videos

--- src/pages/videos.page.jsx
+++ src/pages/videos.page.jsx
@@ -1,8 +1,16 @@
 import { ApiService } from '../services/api.service.js';
 import { ContainerComponent } from '../shared/container.component.jsx';
+import { EditComponent } from '../shared/edit.component.jsx';
 import { PaginationComponent } from '../shared/pagination.component.jsx';
 
-const VideoPageElement = ({ videoElement, className = '' }) => {
+const VideoPageElement = ({ videoElement, refreshVideos, isAdmin, className = '' }) => {
+
+	const [zz, setState] = React.useState();
+
+	const deleteVideo = React.useCallback(() => {
+		ApiService.deleteType('videos', videoElement).then(refreshVideos);
+	}, []);
+
 	return <div className={"card grey darken-4 videos-list " + className} style={{ width: '100em' }}>
 		<div className="card-image waves-effect waves-block waves-light">
 			<div className="video-container">
@@ -10,8 +18,18 @@ const VideoPageElement = ({ videoElement, className = '' }) => {
 			</div>
 		</div>
 		<div className="card-content white-text">
-			<p>{videoElement.auteur} - {videoElement.titre}</p>
+
+			<p>
+				<EditComponent obj={videoElement} keyObj='auteur' type='videos' refresh={refreshVideos} isAdmin={isAdmin} />
+				{' - '}
+				<EditComponent obj={videoElement} keyObj='titre' type='videos' refresh={refreshVideos} isAdmin={isAdmin} />
+				{isAdmin && 'id' in videoElement && <i className="material-icons right" onDoubleClick={deleteVideo}>delete</i>}</p>
 		</div>
+		{
+			isAdmin && <div className="card-content white-text">
+				<p>(<EditComponent obj={videoElement} keyObj='url' type='videos' refresh={refreshVideos} isAdmin={isAdmin} setState={setState} />)</p>
+			</div>
+		}
 	</div>
 };
 
@@ -24,22 +42,35 @@ export const VideosPage = () => {
 	const [currentPage, setCurrentPage] = React.useState(1);
 
 	React.useEffect(() => {
-		ApiService.getNumberOfPages('videos', ELEMENTS_BY_PAGE).then(setCountPages).then(() => refreshVideos(currentPage));
+		ApiService.getNumberOfPages('videos', ELEMENTS_BY_PAGE).then(async a => {
+			if (await ApiService.isAdmin()) {
+				a++;
+			}
+			setCountPages(a);
+		}).then(() => refreshVideos(currentPage));
 	}, []);
 
 	const refreshVideos = React.useCallback(async (page) => {
-		const list = await ApiService.getAll(`videos?page=${page}&count=${ELEMENTS_BY_PAGE}`);
+		const [list, isAdmin] = await Promise.all([ApiService.getAll(`videos?page=${page}&count=${ELEMENTS_BY_PAGE}`), ApiService.isAdmin()]);
+		if (isAdmin && !list.length) {
+			list.push({
+				auteur: "Auteur",
+				titre: "Titre",
+				description: "Description",
+				url: "https://www.youtube.com/embed/JLO4IoWCk-8",
+			});
+		}
 		const out = [];
 		let temp = [];
 		list.forEach((element, index) => {
-			temp.push(<VideoPageElement videoElement={element} className={index % 2 ? 'not-on-phone' : ''} />)
+			temp.push(<VideoPageElement refreshVideos={refreshVideos.bind(undefined, page)} isAdmin={isAdmin} videoElement={element} className={index % 2 ? 'not-on-phone' : ''} />)
 			if (temp.length === 2) {
 				out.push(<React.Fragment>
 					<ContainerComponent>
 						{temp.concat([])}
 					</ContainerComponent>
 					<ContainerComponent className='only-on-phone'>
-						<VideoPageElement videoElement={element} />
+						<VideoPageElement refreshVideos={refreshVideos.bind(undefined, page)} isAdmin={isAdmin} videoElement={element} />
 					</ContainerComponent>
 				</React.Fragment>);
 				temp = [];
@@ -52,6 +83,7 @@ export const VideosPage = () => {
 				</ContainerComponent>
 			</React.Fragment>);
 		}
+		setVideos([]);
 		setVideos(out);
 	}, []);
 
