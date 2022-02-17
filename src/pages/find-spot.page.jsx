9a5b401cd44c27ea1eb11b987956d258d53ccd70
Gauthier <gauthier.vdk@gmail.com>
2022-02-17 16:58:04 +0100

    modif text findspot

--- src/pages/find-spot.page.jsx
+++ src/pages/find-spot.page.jsx
@@ -3,7 +3,7 @@ import { ContainerComponent } from '../shared/container.component.jsx';
 const MapComponent = ({ className = '' }) => {
     return <div className={'card ' + className} style={{ width: '100%' }}>
         <iframe src="https://www.google.com/maps/d/embed?mid=1Lo-6WJ4PTdel36xu_ruBH3LKNRXnuSyV"
-            style={{ display: 'block', width: '100%', border: 'none' }}></iframe>
+            style={{ display: 'block', width: '100%', border: 'none', height: '100%' }}></iframe>
     </div>
 };
 
@@ -12,10 +12,11 @@ export const FindSpotPage = () => {
         <ContainerComponent>
             <div class="card transparent">
                 <div class="card-content">
-                    <span class="card-title grey-text text-darken-3">Spots de Skim</span>
-										<p style={{marginTop: '2%'}}>Voilà une carte des meilleurs spots de skimboard en France</p>
+                    <span class="card-title grey-text text-darken-1" style={{fontWeight: 'bold'}}>Spots de Skim</span>
+                    <p style={{marginTop: '2%'}}>En règle générale un spot de skim de vague est une plage en pente (qui favorisera la création de shorebreak, vagues cassant au bord).</p>
+										<p style={{marginTop: '2%'}}>Nous vous proposons ici une carte des meilleurs spots de skimboard en France !</p>
 										<p style={{marginTop: '2%'}}>Tous ces spots ont été testé et approuvé par un skimboarder habitué du spot, pour pouvoir vous donner un max d'infos
-											sur les conditions propices à une session de rêve sur ce spot</p>
+											sur les conditions propices à une session de rêve sur ce spot.</p>
 										<p style={{marginTop: '2%'}}>Envie de partager votre spot ?</p>
                 </div>
                 <div class="card-action">
